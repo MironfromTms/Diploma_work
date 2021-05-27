51f1a6bd7658d870ae8925c8f9520a00de07d06d
Юрий Миронов <mironov.y70@gamil.com>
2021-05-27 14:25:52 +0700

    Исправил 7 задание

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,6 +1,6 @@
-numbers = int(input('Введите целое число: '))
+number = int(input('Введите целое число: '))
 list = []
-for i in range(1, numbers+1):
+for i in range(1, number+1):
     if i % 2 != 0:
         list.append(i)
 print(list)
