a7b651a53052f58d09817914c903863760bffc46
Юрий Миронов <mironov.y70@gamil.com>
2022-01-27 12:39:09 +0700

    Выполнил вторую задачу задачу

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+first_class = []
+for i in range(160, 177, 2):
+    first_class.append(i)
+
+print(first_class)
+
+secound_class = []
+
+for i in range(162, 181, 3):
+    secound_class.append(i)
+
+print(secound_class)
+
+new_class = first_class+secound_class
+new_class.sort()
+print(new_class)
