65dcee8aa0095b6aab492e15ed28378cd8ca33f3
Юрий Миронов <mironov.y70@gamil.com>
2022-02-02 18:53:45 +0700

    9 полностью сам сделала без подсказок. в 100 раз проще 8 задачи

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -3,13 +3,22 @@ print('Кол-во друзей: ', end='')
 friends = int(input())
 print('Долговых расписок: ', end='')
 receipt = int(input())
-i = 1
-while receipt < 0:
-    receipt -= 1
-    print(i, 'расписка')
+friends_list = []
+
+for _ in range(friends):
+    friends_list.append([0, 0])
+
+for i in range(receipt):
+    print(i + 1, 'расписка')
     print('Кому: ', end='')
     to_who = int(input())
     print('От кого: ', end='')
     from_who = int(input())
     print('Сколько: ', end='')
     loan = int(input())
+    friends_list[to_who - 1][1] += loan
+    friends_list[from_who - 1][1] -= loan
+
+print('Баланс друзей:')
+for i in range(friends):
+    print(i + 1, ':', friends_list[i][1])
