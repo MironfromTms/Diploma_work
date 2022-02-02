06b881aee8eccbe0e567f687cf4605f95a59c8ae
Юрий Миронов <mironov.y70@gamil.com>
2022-02-02 14:56:58 +0700

    8 задачу вообще не понял. Спасибо за подсказку, но это прям мимо меня

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -4,30 +4,20 @@ people = int(input())
 print('Какое число в считалке? ', end='')
 rhyme_number = int(input())
 print('Значит, выбывает каждый', rhyme_number, 'человек')
-people_list = []
 print()
 
-for i in range(people):
-    people_list.append(i + 1)
+people_list = list(range(1, people + 1))
+rhyme_number = 7
 
-print('Текущий круг людей:', people_list)
-print('Начало счёта с номера ', people_list[0])
-print('Выбывает человек под номером ', rhyme_number % len(people_list))
-people_list.remove(people_list[rhyme_number % len(people_list)-1])
-print()
-
+start = 0
 while len(people_list) > 1:
-    out = rhyme_number % len(people_list)-1
-    if out == 0:
-        print('Текущий круг людей:', people_list)
-        print('Начало счёта с номера ', people_list[out])
-        print('Выбывает человек под номером ', people_list[out])
-        people_list.remove(people_list[out])
-        print()
-    else:
-        print('Текущий круг людей:', people_list)
-        print('Начало счёта с номера ', people_list[out-1])
-        print('Выбывает человек под номером ', people_list[out]+1)
-        people_list.remove(people_list[out]+1)
-        print()
-print('Остался человек под номером ', people_list[0])
+    print('Текущий круг людей:', people_list)
+    print('Начало счёта с номера ', people_list[start])
+    start += rhyme_number - 1
+    start = start % len(people_list)
+    print('Выбывает человек под номером ', people_list[start])
+    people_list.remove(people_list[start])
+    start = start % len(people_list)
+    print()
+print('Остался человек под номером ', people_list)
+
--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+print('Кол-во друзей: ', end='')
+friends = int(input())
+print('Долговых расписок: ', end='')
+receipt = int(input())
+i = 1
+while receipt < 0:
+    receipt -= 1
+    print(i, 'расписка')
+    print('Кому: ', end='')
+    to_who = int(input())
+    print('От кого: ', end='')
+    from_who = int(input())
+    print('Сколько: ', end='')
+    loan = int(input())
