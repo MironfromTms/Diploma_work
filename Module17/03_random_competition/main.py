9a17e03857f353be7ea6f02ebc0ee1c49e279027
Юрий Миронов <mironov.y70@gamil.com>
2022-02-07 18:23:46 +0700

    3 task is done

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+import random
+
+team_1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+team_2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+winners = [(team_1[x] if team_1[x] > team_2[x]
+            else team_2[x]) for x in range(20)]
+print('Первая команда: ', team_1)
+print('Вторая команда: ', team_2)
+print('Победители тура: ', winners)
