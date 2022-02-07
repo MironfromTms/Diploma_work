ecfef5f53e93c862fa4d4f4f0881df55ce9c46ad
Юрий Миронов <mironov.y70@gamil.com>
2022-02-07 18:08:42 +0700

    1 task is done

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+phrase = input('Введите текст:')
+vowels = ['а', 'у', 'и', 'е', 'ё', 'ы', 'о', 'э', 'я', 'ю']
+vowels_in_phrase = []
+for letter in phrase:
+    if letter in vowels:
+        vowels_in_phrase.append(letter)
+
+print('Список гласных букв: ', vowels_in_phrase)
+print('Длина списка: ', len(vowels_in_phrase))
--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,1 +1,5 @@
 # TODO здесь писать код
+len_list = int(input('Введите длину списка: '))
+num_list = [x for x in range(len_list)]
+print(num_list)
+
