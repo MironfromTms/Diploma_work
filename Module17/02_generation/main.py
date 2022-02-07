6791c7f8e8a736f6e3a9579cdeadf8d3450689c7
Юрий Миронов <mironov.y70@gamil.com>
2022-02-07 18:27:03 +0700

    2 task is done

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,5 +1,6 @@
 # TODO здесь писать код
 len_list = int(input('Введите длину списка: '))
-num_list = [x for x in range(len_list)]
-print(num_list)
+num_list = [(1 if x % 2 == 0
+            else x % 5)for x in range(len_list)]
 
+print('Результат: ', num_list)
