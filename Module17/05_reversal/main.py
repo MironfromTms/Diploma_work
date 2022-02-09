8088454c79430faf2f1f60421aae89a460c44eee
Юрий Миронов <mironov.y70@gamil.com>
2022-02-09 12:12:01 +0700

    5 task is done

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,5 @@
 # TODO здесь писать код
+phrase = input('Введите строку: ')
+start = phrase.index('h')
+stop = phrase.rindex('h')
+print('Развернутая последовательность между первым и последним h:', phrase[stop - 1:start:-1])
--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,1 +1,3 @@
 # TODO здесь писать код
+list = [[x for x in range(1, 4)] for i in range(4)]
+print(list)
