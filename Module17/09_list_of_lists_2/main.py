48f7eb130e1b29b6c67be913bc98f1074388a18d
Юрий Миронов <mironov.y70@gamil.com>
2022-02-13 23:45:05 +0700

    9 task is done

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,5 +1,5 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
-open_nice_list = [x for x in nice_list]
+open_nice_list = [num for col in nice_list for row in col for num in row]
 print(open_nice_list)
 # TODO здесь писать код
