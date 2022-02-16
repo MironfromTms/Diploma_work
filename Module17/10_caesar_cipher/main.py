1645a607020e122ae8139fad2222ddf26c0bea04
@mironov_iurii <mirononv.y70@gmail.com>
2022-02-17 00:01:19 +0700

    10 task has done

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+def ceasar_cipher(string, user_step):
+    char_list = [(alphabet[(alphabet.index(sym) + user_step) % 33] if sym != ' ' else ' ') for sym in string]
+    new_str = ''
+    for i_char in char_list:
+        new_str += i_char
+    return new_str
+
+
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+input_str = input('Введите строку: ')
+step = int(input('Введите шаг сдвига: '))
+output_str = ceasar_cipher(input_str, step)
+
+print('Зашифрованная строка: ', output_str)
