35ca6bdbd4663d7b44bf4b429701513f28ed1800
@mironov_iurii <mirononv.y70@gmail.com>
2022-02-17 11:23:50 +0700

    переделал 1 задание

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,10 +1,7 @@
 # TODO здесь писать код
 phrase = input('Введите текст:')
 vowels = ['а', 'у', 'и', 'е', 'ё', 'ы', 'о', 'э', 'я', 'ю']
-vowels_in_phrase = []
-for letter in phrase:
-    if letter in vowels:
-        vowels_in_phrase.append(letter)
+vowels_in_phrase = [letter for letter in phrase if letter in vowels]
 
 print('Список гласных букв: ', vowels_in_phrase)
 print('Длина списка: ', len(vowels_in_phrase))
