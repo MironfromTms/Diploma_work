1e01bf2c80c9e24c2e67f0abf9c8390e9f818c90
@mironov_iurii <mirononv.y70@gmail.com>
2022-02-28 15:10:06 +0700

    2 task has done

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+text = input('Введите строку:').split()
+maximum = 0
+maximum_word = []
+for i_text in text:
+    if len(i_text) > maximum:
+        maximum_word = i_text
+print('Самое длинное слово:', maximum_word)
+print('Длина этого слова:', len(maximum_word))
+
