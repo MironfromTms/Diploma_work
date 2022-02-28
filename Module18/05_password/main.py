2a87e798bae0891ef4c3a796804e8ec5a7881a55
@mironov_iurii <mirononv.y70@gmail.com>
2022-02-28 16:25:40 +0700

    5 task has done

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+while True:
+    password = input('Придумайте пароль:', )
+    num_counter = 0
+    upper_count = 0
+    for i_num in password:
+        if i_num.isdigit():
+            num_counter += 1
+    for i_let in password:
+        if i_let.isupper():
+            upper_count += 1
+    if len(password) > 8 and upper_count > 0 and num_counter >= 3:
+        print('Это надёжный пароль!')
+        break
+    else:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+
+
