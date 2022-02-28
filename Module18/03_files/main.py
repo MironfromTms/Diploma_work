44b7c9b4d768e6fb9b94b80bb0afdb78f8775212
@mironov_iurii <mirononv.y70@gmail.com>
2022-02-28 14:00:29 +0700

    3 task has done

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+file_name = input('Название файла:')
+error_symbols = '@№$%^&*()'
+files_ends = ['.txt, .docx']
+for i_sym in error_symbols:
+    if file_name.startswith(i_sym):
+        print('Ошибка: название начинается на один из специальных символов')
+for i_sym in files_ends:
+    if file_name.endswith(i_sym):
+        print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+
+print('Файл назван верно.')
