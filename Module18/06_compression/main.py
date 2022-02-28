b3150e2777cd168765ecca21cf1feb664040ae5f
@mironov_iurii <mirononv.y70@gmail.com>
2022-02-28 20:06:23 +0700

    6 task has done

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,14 @@
 # TODO здесь писать код
+phrase = list(input('Введите строку: '))
+count = 1
+new_phrase = []
+for i in range(1, len(phrase)):
+    if phrase[i-1] == phrase[i]:
+        count += 1
+    else:
+        new_phrase.append(phrase[i-1])
+        new_phrase.append(str(count))
+        count = 1
+new_phrase.append(phrase[-1])
+new_phrase.append(str(count))
+print('Новое сообщение: ', ''.join(new_phrase))
