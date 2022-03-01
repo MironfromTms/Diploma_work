2e467c444a97e0d5cd072a622311c4cddf10ba0c
@mironov_iurii <mirononv.y70@gmail.com>
2022-03-01 20:54:36 +0700

    8 task has done

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,21 @@
 # TODO здесь писать код
+def shift(string):
+    new_str = string[-1]
+    for i in range(len(string)-1):
+        new_str += string[i]
+    return new_str
+
+
+first_str = input('Первая строка: ')
+second_str = input('Вторая строка: ')
+if len(first_str) == len(second_str):
+    step = 0
+    for i in range(len(first_str)):
+        if first_str == second_str:
+            print(f' Первая строка получается из второй со сдвигом {step}.')
+            break
+        else:
+            second_str = shift(second_str)
+            step += 1
+    if len(second_str) == step:
+        print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
