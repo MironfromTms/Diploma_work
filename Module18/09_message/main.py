9859e3d45274959502449ab2dd98dc55a8ac02b1
@mironov_iurii <mirononv.y70@gmail.com>
2022-03-02 22:41:54 +0700

    9 task has done

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+
+def reversed_word(word):
+    buffer = []
+    result = []
+    for i in word:
+        if i.isalpha():
+            buffer.append(i)
+        else:
+            rev_buffer = buffer[::-1]
+            result.append(rev_buffer)
+            result.append(i)
+            buffer = []
+    open_result = [j for i in result for j in i]
+    str_result = ''.join(open_result)
+    print(str_result)
+
+
+
+word = input('Сообщение: ')
+print('Новое сообщение: ', end='')
+reversed_word(word)
