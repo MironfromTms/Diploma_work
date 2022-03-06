4c8d09363fe5268343eb2358f2d791805fbc9cbe
mironov.y70@gmail.com <Olesya19101987>
2022-03-07 01:11:32 +0700

    10 task has done. I'm happy)

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,26 +1,28 @@
 
 def ceasar_cipher(string):
-    char_list = [(alphabet[(alphabet.index(sym) + 1) % 52] if sym.isalpha() else '\n') for sym in string]
+    char_list = [(alphabet[(alphabet.index(sym) - 1) % 52] if sym.isalpha() else ' ') for sym in string]
     new_str = ''
     for i_char in char_list:
         new_str += i_char
     return new_str
 
 
-def shift(string, step=3):
+def shift(string, step):
     string_list = string.split()
     new_string = []
     for i_word in string_list:
-        new_word = i_word[-step:] + i_word[:-step]
+        if len(i_word) < step:
+            new_step = step % len(i_word)
+            new_word = i_word[-new_step:] + i_word[:-new_step]
+        else:
+            new_word = i_word[-step:] + i_word[:-step]
         new_string.append(new_word)
-        if '/' in i_word:
-            step += 1
-    answer = ' '.join(new_string)
-    print()
-    return answer
+    return new_string
 
 
-message = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm '
+
+
+message = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
 alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 message_list = message.split()
 buffer = []
@@ -31,12 +33,13 @@ for i_word in message_list:
         str_buffer = ' '.join(buffer)
         answer.append(str_buffer)
         buffer = []
+print('Зашифрованная строка: ')
+step = 3
 for i in answer:
     str_i = ''.join(i)
-    beauty = shift(str_i)
-    print(beauty)
+    beauty = shift(str_i, step)
+    beaty_answer = ' '.join(beauty)
+    uncode_answer = ceasar_cipher(beaty_answer)
+    step += 1
+    print(uncode_answer)
 
-
-
-
-# print('Зашифрованная строка: ', )
