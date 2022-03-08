c716bea9cf85e652bb5103ad9878d490fdad099f
mironov.y70@gmail.com <Olesya19101987>
2022-03-08 12:38:54 +0700

    agg 'черновик'

--- черновик дз.py
+++ черновик дз.py
@@ -87,3 +87,96 @@
 #                 index = violator_songs.index(n)
 #                 total += violator_songs[index][1]
 # print('Общее время звучания песен: ', total)
+
+# while True:
+#     contacts = {}
+#     for i in range(1000):
+#         print('Текущие контакты на телефоне:', contacts)
+#         print()
+#         name = input('Введите имя: ')
+#         if name in contacts:
+#             print('Ошибка, имя уже есть в списке контактов.')
+#             continue
+#         phone_number = input('Введите номер телефона: ')
+#         contacts[name] = phone_number
+
+
+# small_storage = {
+#     'гвозди': 5000,
+#     'шурупы': 3040,
+#     'саморезы': 2000
+# }
+# big_storage = {
+#     'доски': 1000,
+#     'балки': 150,
+#     'рейки': 600
+# }
+# big_storage.update(small_storage)
+# print(big_storage)
+#
+# goods = input('Введите название товара: ')
+# if goods in big_storage:
+#     print(big_storage.get(goods))
+# else:
+#     print('Такого товара нет в списке')
+
+# incomes = {
+#     'apple': 5600.20,
+#     'orange': 3500.45,
+#     'banana': 5000.00,
+#     'bergamot': 3700.56,
+#     'durian': 5987.23,
+#     'grapefruit': 300.40,
+#     'peach': 10000.50,
+#     'pear': 1020.00,
+#     'persimmon': 310.00,
+# }
+
+# summ = 0
+# for i in incomes.values():
+#     summ += i
+# print('Общий доход за год составил: ', summ)
+# min_value = min(incomes.values())
+# sorted_values = sorted(incomes.values())
+# print('Самый маленький доход у ', min(incomes, key=incomes.get), '. Он составляет ', min_value)
+# incomes.pop(min(incomes, key=incomes.get))
+# print('Итоговый словарь: ', incomes)
+#
+# players_dict = {
+#     1: {'name': 'Vanya', 'team': 'A', 'status': 'Rest'},
+#     2: {'name': 'Lena', 'team': 'B', 'status': 'Training'},
+#     3: {'name': 'Maxim', 'team': 'C', 'status': 'Travel'},
+#     4: {'name': 'Egor', 'team': 'C', 'status': 'Rest'},
+#     5: {'name': 'Andrei', 'team': 'A', 'status': 'Training'},
+#     6: {'name': 'Sasha', 'team': 'A', 'status': 'Rest'},
+#     7: {'name': 'Alina', 'team': 'B', 'status': 'Rest'},
+#     8: {'name': 'Masha', 'team': 'C', 'status': 'Travel'}
+# }
+#
+# team_member = [
+#     player['name']
+#     for player in players_dict.values()
+#     if player['team'] == 'C' and player['status'] == 'Travel'
+# ]
+#
+# print(team_member)
+
+family_member = {
+    "name": "Jane",
+    "surname": "Doe",
+    "hobbies": ["running", "sky diving", "singing"],
+    "age": 35,
+    "children": [
+        {
+            "name": "Alice",
+            "age": 6
+        },
+        {
+            "name": "Bob",
+            "age": 8
+        }
+    ]
+}
+for i in family_member.keys():
+    print(i, '-', family_member.get(i))
+print(family_member['children'][1].get('name'))
