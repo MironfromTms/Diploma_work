3b1f89e73b0245c819b951bdd6e799a3e2c2366d
@mironov_iurii <mirononv.y70@gmail.com>
2022-03-16 14:36:06 +0700

    2 task has done

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+countries = int(input('Кол-во стран: '))
+country_dict = dict()
+for i in range(1, countries + 1):
+    print(i, 'страна: ', end='')
+    country = input()
+    country_info = country.split()
+    for j in country_info[1:]:
+        country_dict[j] = country_info[0]
+
+print()
+
+for i in range(1, 4):
+    print(i, 'город: ', end='')
+    city = input()
+    if city in country_dict:
+        print('Город', city, 'расположен в стране', country_dict[city])
+    else:
+        print('По городу', city, 'данных нет.')
