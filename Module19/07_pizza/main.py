edf4502327b5e6c8405b96d4ee14d0cef75ae03b
@mironov_iurii <mirononv.y70@gmail.com>
2022-03-16 23:31:22 +0700

    7 task has done

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+orders_number = int(input('Введите кол-во заказов: '))
+orders_dict = dict()
+for i in range(1, orders_number + 1):
+    print(i, 'заказ: ', end='')
+    surname, pizza, quantity = input().split()
+    if surname not in orders_dict:
+        orders_dict[surname] = {pizza: int(quantity)}
+    else:
+        if pizza not in orders_dict[surname]:
+            orders_dict[surname][pizza] = int(quantity)
+        else:
+            orders_dict[surname][pizza] += int(quantity)
+
+for i in orders_dict:
+    print(i, ':')
+    for j in orders_dict[i]:
+        print('        ', j, ':', orders_dict[i][j])
