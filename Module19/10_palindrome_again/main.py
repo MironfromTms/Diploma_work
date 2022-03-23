a8b56f1ce9107afe5a4088e472f64846e3ff4d12
@mironov_iurii <mirononv.y70@gmail.com>
2022-03-23 16:36:51 +0700

    10 task has done

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+def is_polyndrom(string):
+    char_dict = dict()
+    for i in string:
+        char_dict[i] = char_dict.get(i, 0) + 1
+
+    odd_count = 0
+    for j in char_dict.values():
+        if j % 2 != 0:
+            odd_count += 1
+
+    return odd_count
+
+phrase = input('Введите строку: ')
+if is_polyndrom(phrase):
+    print('Можно сделать палиндромом')
+else:
+    print('Нельзя сделать палиндромом')
