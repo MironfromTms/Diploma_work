111a6bb14be0c567b5aac2059d35a4241d440f27
@mironov_iurii <mirononv.y70@gmail.com>
2022-03-23 16:13:38 +0700

    9 task has done

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+people = int(input('Введите количество человек: '))
+family_dict = dict()
+cointer = 0
+for i in range(1, people + 1):
+    print(i, 'пара:', end='')
+    pair = input()
+    pair_list = pair.split()
+    if pair_list[1] not in family_dict:
+        family_dict[pair_list[1]] = 0
+        family_dict[pair_list[0]] = family_dict[pair_list[1]] + 1
+    else:
+        family_dict[pair_list[0]] = family_dict[pair_list[1]] + 1
+print('“Высота” каждого члена семьи:')
+for i in sorted(family_dict.keys()):
+    print(i, family_dict[i])
+
