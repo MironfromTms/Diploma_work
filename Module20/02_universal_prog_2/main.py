a00cd1a5ff05a56c8f272c84625e503fec49fbdc
mironov.y70@gmail.com <Olesya19101987>
2022-04-14 20:08:50 +0700

    2 task has done

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+def is_prime(number):
+    if number > 1:
+        for i in range(2, (number // 2) + 1):
+            if number % i == 0:
+                return False
+        return True
+
+
+def crypto(string):
+    answer = list()
+    for j_index, j_sym in enumerate(string):
+        if is_prime(j_index):
+            answer.append(j_sym)
+    return answer
+
+
+print(crypto([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(crypto('О Дивный Новый мир!'))
