d97ad685c2107f8a276b21354b6b0580a87ba1d6
mironov.y70@gmail.com <Olesya19101987>
2022-04-14 19:21:00 +0700

    6 task has done

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,1 +1,9 @@
 # TODO здесь писать код
+original_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+n = 0
+new_list = list()
+for i_number in range(5):
+    new_list.append(tuple(original_list[n:n+2]))
+    n += 2
+print(new_list)
+
