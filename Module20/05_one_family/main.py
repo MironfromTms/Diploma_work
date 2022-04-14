e2b6768cb6a489d8632679d225bc91f50df4e960
mironov.y70@gmail.com <Olesya19101987>
2022-04-14 15:24:16 +0700

    5 task has done

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+families = {('Сидоров', 'Никита'): 35,
+            ('Сидорова', 'Алина'): 33,
+            ('Сидоров', 'Павел'): 10,
+            ('Иванов', 'Григорий'): 42,
+            ('Иванова', 'Анна'): 39,
+            ('Петров', 'Алесандр'): 33,
+            ('Петрова', 'Алесандра'): 23,
+            }
+
+user_family = input('Введите фамилию: ')
+
+for i_name, i_age in families.items():
+    if user_family == i_name[0]:
+        print(i_name[0], i_name[1], i_age)
+    elif i_name[0][:-1] == user_family:
+        print(i_name[0], i_name[1], i_age)
