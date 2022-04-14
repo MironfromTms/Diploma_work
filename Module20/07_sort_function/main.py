62049fbdd9c27e03b570786930ab05b5139968ac
mironov.y70@gmail.com <Olesya19101987>
2022-04-14 20:23:13 +0700

    7 task has done

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+def tpl_sort(user_tuple):
+    user_list = list(user_tuple)
+    sorted_user_list = sorted(user_list)
+    sorted_tuple = tuple(sorted_user_list)
+    for i in sorted_user_list:
+        if int(i) != i:
+            return user_tuple
+    return sorted_tuple
+
+
+my_tuple = (6, 3, -1, 8, 4, 10, -5)
+print(tpl_sort(my_tuple))
