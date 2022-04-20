ed75234841fc1a9f8be7f90bd040b8ed62044e7b
@mironov_iurii <mirononv.y70@gmail.com>
2022-04-20 13:16:08 +0700

    10 task has done

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+def shortes_range(string, tpl):
+    return min(len(string), len(tpl))
+
+
+syms_str = 'abcd'
+nums_tpl = (10, 20, 30, 40)
+
+pairs = ((syms_str[i_elem], nums_tpl[i_elem]) for i_elem in range(shortes_range(syms_str, nums_tpl)))
+print(pairs)
+for i in pairs:
+    print(i)
