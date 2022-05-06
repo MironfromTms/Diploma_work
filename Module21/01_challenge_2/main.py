e8f93c0b5b23ecd8d3a1a5bf179e7ec6730cda02
@mironov_iurii <mirononv.y70@gmail.com>
2022-05-06 13:50:31 +0700

    1 task has done

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def num_count(number):
+    if number == 0:
+        return 1
+    num_count(number - 1)
+    print(number)
+
+
+user_number = int(input('Введите num: '))
+num_count(user_number)
