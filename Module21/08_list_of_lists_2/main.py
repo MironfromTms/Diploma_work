226429a9602c520a1ab4f279d211b6162299961a
@mironov_iurii <mirononv.y70@gmail.com>
2022-05-11 15:45:59 +0700

    8 task has done

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,19 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
+
 # TODO здесь писать код
+
+def my_list(*args):
+    answer = list()
+    for elem in args:
+        for sub_elem in elem:
+            if not isinstance(sub_elem, list):
+                answer.append(sub_elem)
+            else:
+                result = my_list(sub_elem)
+                answer.extend(result)
+    return answer
+
+
+print(my_list(nice_list))
