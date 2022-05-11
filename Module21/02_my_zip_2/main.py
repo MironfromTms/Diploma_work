71cc938b03507d9aa6668023ce22083380ad80be
@mironov_iurii <mirononv.y70@gmail.com>
2022-05-11 14:11:24 +0700

    2 task has done

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,5 @@
 # TODO здесь писать код
+def my_zip(*args):
+    length = min(len(element) for element in args)
+    tpl_list = [tuple(struct[i] for struct in args) for i in range(length)]
+    return tpl_list
