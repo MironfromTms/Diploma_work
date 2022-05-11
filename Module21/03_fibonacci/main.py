3ebb7f1dcaf6864610f148e6b52073f69c44bc37
@mironov_iurii <mirononv.y70@gmail.com>
2022-05-11 11:17:21 +0700

    3 task has done

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,9 @@
 # TODO здесь писать код
+def position_fibonacci(number):
+    if number == 1 or number == 2:
+        return 1
+    return position_fibonacci(number - 1) + position_fibonacci(number - 2)
+
+
+user_number = int(input('Введите позицию числа в ряде Фибоначчи: '))
+print('Число:', position_fibonacci(user_number))
