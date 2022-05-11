b1455fc76ce79ccba8e4d77c13fa215016a2ef48
@mironov_iurii <mirononv.y70@gmail.com>
2022-05-11 23:34:52 +0700

    5 task has done

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,25 @@
-def calculating_math_func(data):
+# def calculating_math_func(data):
+#     result = 1
+#     for index in range(1, data + 1):
+#         result *= index
+#     result /= data ** 3
+#     result = result ** 10
+#     return result
+
+
+def calculating_math_func(data, user_dict=dict()):
     result = 1
-    for index in range(1, data + 1):
-        result *= index
+    for i in range(1, data + 1):
+        result *= i
+        user_dict[i] = result
+        print(user_dict)
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+
+print(calculating_math_func(5))
+print(calculating_math_func(10))
+print(calculating_math_func(15))
+print(calculating_math_func(3))
+
