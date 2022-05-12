77860f3bf70cabb8e45f1901f4744ee8594375ba
@mironov_iurii <mirononv.y70@gmail.com>
2022-05-12 09:35:10 +0700

    7 task has done

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+def my_list(*args):
+    answer = list()
+    for elem in args:
+        for sub_elem in elem:
+            if not isinstance(sub_elem, list):
+                answer.append(sub_elem)
+            else:
+                result = my_list(sub_elem)
+                answer.extend(result)
+    return answer
+
+
+numbers_1 = ([[1, 2, [3]], [1], 3])
+numbers_2 = (1, 2, 3, 4, 5)
+print(sum(my_list(numbers_1)))
+print(sum(my_list(numbers_2)))
