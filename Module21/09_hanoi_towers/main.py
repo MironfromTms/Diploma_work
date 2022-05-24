7e031f90bda98b07b857ee07e277c23e9851bee8
@mironov_iurii <mirononv.y70@gmail.com>
2022-05-24 12:08:10 +0700

    9 task has done

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+
+def moves(steps, x=1, y=3):
+    if steps == 1:
+        print(f'Переложит диск {steps} со стержня номер {x} на стержень номер {y}')
+    else:
+        moves(steps - 1, x, 6 - x - y)
+        print(f'Переложит диск {steps} со стержня номер {x} на стержень номер {y}')
+        moves(steps - 1, 6 - x - y, y)
+
+
+disc_number = int(input('Введите количество дисков:'))
+moves(disc_number)
