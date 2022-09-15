7436b9dad5009bb3b60f62e491b3ebbe6058e3a8
@mironov_iurii <mirononv.y70@gmail.com>
2022-09-15 16:57:43 +0700

    1 task has done.

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+numbers_file = open('numbers.txt', 'r')
+summ_of_numbers = []
+for i_number in numbers_file:
+    info = i_number.split()
+    summ_of_numbers.append(info)
+    summ_of_numbers_list = [i for y in summ_of_numbers for i in y]
+int_summ = 0
+for i in summ_of_numbers_list:
+    int_summ += int(i)
+answer = open('answer.txt', 'w')
+answer.write(str(int_summ))
+
+print('Содержимое файла answer.txt')
+print(int_summ)
+numbers_file.close()
+answer.close()
