361f19453a3adc5650aa314e1e2dd5fd3690d4ed
@mironov_iurii <mirononv.y70@gmail.com>
2022-09-15 19:55:58 +0700

    2 task has done.

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+zen_text = open('zen.txt', 'r', encoding='utf8')
+zen_text_list = []
+for i_str in zen_text:
+    zen_text_list.append(i_str)
+for i_str in zen_text_list[::-1]:
+    print(i_str, end='')
