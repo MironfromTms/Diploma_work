3c465db3339ba302560b09c151c8ccd969e676c6
mironov.y70@gmail.com <Olesya19101987>
2022-10-12 14:48:59 +0600

    1 task has done

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+result = 0
+line_count = 0
+
+with open('people.txt', 'r', encoding='utf8') as people:
+    for i_line in people:
+        line_len = len(i_line)
+        result += line_len
+        if i_line.endswith('\n'):
+            result -= 1
+            line_count += 1
+            try:
+                if line_len <= 3:
+                    raise TypeError
+            except TypeError:
+                print('Ошибка: менее трёх символов в строке {}.'.format(line_count))
+print('Общее количество символов: ', result)
