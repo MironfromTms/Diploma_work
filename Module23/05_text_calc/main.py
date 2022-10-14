8d6521e9fe37fb13da0bb6aae9d0430e223eb0aa
mironov.y70@gmail.com <Olesya19101987>
2022-10-14 15:47:31 +0600

    5 task has done

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,12 @@
 # TODO здесь писать код
+with open('calc.txt', 'r') as file:
+    result = []
+    for i_line in file:
+        try:
+            result.append(eval(i_line))
+        except:
+            user_answer = input('Обнаружена ошибка в строке :{} Хотите исправить?'.format(i_line)).lower()
+            if user_answer == 'yes' or user_answer == 'да':
+                i_line = input('Введите исправленную строку:')
+                result.append(eval(i_line))
+    print('Сумма результатов: ', round(sum(result), 2))
