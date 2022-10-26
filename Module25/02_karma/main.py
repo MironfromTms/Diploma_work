1d8c5a40b0d50e687eeafdeb0d12ebe393f2a627
mironov.y70@gmail.com <Olesya19101987>
2022-10-26 10:39:22 +0600

    2 task has done

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+import random
+
+carma = 0
+day = 1
+
+
+def one_day(number):
+    if random.randint(1, 10) == 10:
+        with open('carma.log', 'a', encoding='utf8') as carma_file:
+            misdemeanor = random.choices(['KillError', 'DrunkError', 'CarCrashError', 'GluttonyError',
+                                          'DepressionError'])
+            carma_file.write(f'At day number {number} the misdemeanor was - {misdemeanor}\n')
+
+
+while carma < 500:
+    one_day(day)
+    day += 1
+    carma += random.randint(1, 7)
+print('You are in nirvana now!')
