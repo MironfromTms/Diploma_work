00db07793057f77f0b5238722f2ad22680203496
mironov.y70@gmail.com <Olesya19101987>
2022-11-09 09:38:38 +0600

    5 task has done

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import functools
+from collections.abc import Callable
+
+
+def counter(func: Callable) -> Callable:
+    """Decorator which counts how many times
+     we called decorating function"""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.count += 1
+        result = func(*args, **kwargs)
+        print(f'Function with name "{func.__name__}" has called {wrapper.count} times.')
+        return result
+
+    wrapper.count = 0
+    return wrapper
+
+
+@counter
+def test():
+    print('Hello')
+
+
+test()
+test()
+test()
