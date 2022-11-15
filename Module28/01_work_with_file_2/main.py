029bad6c221c8e78b2954bf4ab9a35a233d486b1
mironov.y70@gmail.com <Olesya19101987>
2022-11-15 15:16:46 +0600

    1 task has done

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,23 @@
 # TODO здесь писать код
+class File:
+
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        if self.file is True:
+            self.file = open(self.filename, self.mode, encoding='utf8')
+
+        else:
+            self.file = open(self.filename, 'w', encoding='utf8')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        return True
+
+
+with File("example.txt", "w") as file:
+    file.write("Всем привет!")
