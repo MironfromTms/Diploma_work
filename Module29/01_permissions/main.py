e9cc26509b1d23518f0150dbcf6872fef06f66c9
mironov.y70@gmail.com <Olesya19101987>
2022-11-24 17:31:00 +0600

    1 task has done

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+from contextlib import contextmanager
+
+user_permissions = ['admin']
+
+
+def check_permission(name):
+    def wrap_fun(func):
+        def wrapped_func(*args, **kwargs):
+            if name == 'admin':
+                func(*args, **kwargs)
+            else:
+                print('PermissionError: This user can not add comment')
+
+        return wrapped_func
+
+    return wrap_fun
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
