059d1ca4d3c184de9042832df7e160bd567ebfac
mironov.y70@gmail.com <Olesya19101987>
2022-11-25 14:02:07 +0600

    2 task has done

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+import functools
+from typing import Callable, Dict, Any
+
+app = dict()
+
+
+def callback(route: str) -> Callable:
+    def decorator_callback(func: Callable) -> Callable:
+        app[route] = func
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Any:
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator_callback
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
