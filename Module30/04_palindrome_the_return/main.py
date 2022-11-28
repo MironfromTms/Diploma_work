9f4790b9cb0b5dc023674b4b7560df692c0ef391
mironov.y70@gmail.com <Olesya19101987>
2022-11-28 13:45:14 +0600

    4 task has done

--- Module30/04_palindrome_the_return/main.py
+++ Module30/04_palindrome_the_return/main.py
@@ -1,15 +1,9 @@
-from functools import reduce
-from typing import List
+from collections import Counter
 
 
-def my_add(a: int, b: int) -> int:
-    result = a * b
-    return result
+def can_be_poly(val: str) -> bool:
+    return len(list(filter(lambda x: x % 2, Counter(val).values()))) <= 2
 
 
-floats: List[float] = [12.3554, 4.02, 5.777, 2.12, 3.13, 4.44, 11.0001]
-names: List[str] = ["Vanes", "Alen", "Jana", "William", "Richards", "Joy"]
-numbers: List[int] = [22, 33, 10, 6894, 11, 2, 1]
-print(list(map(lambda x: round(x ** 3, 3), floats)))
-print(list(filter(lambda x: len(x) >= 5, names)))
-print(reduce(lambda x, y: x * y, numbers))
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
