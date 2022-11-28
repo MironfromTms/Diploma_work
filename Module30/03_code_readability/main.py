ead759665dc435cd349eb1f6e661ae03d9548c18
mironov.y70@gmail.com <Olesya19101987>
2022-11-28 16:26:23 +0600

    have made some edits in 3 task

--- Module30/03_code_readability/main.py
+++ Module30/03_code_readability/main.py
@@ -1,7 +1,10 @@
-
-print([x for x in range(1001)])
+print(list(filter(lambda x: all(map(lambda y: x % y != 0, range(2, int(x ** 0.5) + 1))), range(2, 1001))))
 
 nums_list = []
-for i in range(1001):
-    nums_list.append(i)
+for i in range(2, 1001):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        nums_list.append(i)
 print(nums_list)
