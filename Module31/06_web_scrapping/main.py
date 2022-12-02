6db07d00b29fcfe8caf82ba42fcd592258e87122
mironov.y70@gmail.com <Olesya19101987>
2022-12-02 11:27:37 +0600

    I have done 5 task

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import requests
+import re
+
+response = requests.get('http://www.columbia.edu/~fdc/sample.html').text
+result_beta = re.findall(r'>.+</h3>', response)
+release = list(map(lambda x: x[1:-5], result_beta))
+print(release)
+
