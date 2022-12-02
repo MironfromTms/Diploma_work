4ba25b068a52b105e8c0791e2e8455638f796ec6
mironov.y70@gmail.com <Olesya19101987>
2022-12-02 17:33:10 +0600

    I have done some edits in 3 task

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -1,6 +1,14 @@
 import requests
 import json
 
+episode_req = requests.get('https://www.breakingbadapi.com/api/episodes')
+episode_data = json.loads(episode_req.text)
+episode_id = 0
+for i_episode in episode_data:
+    if i_episode['season'] == '2' and i_episode['episode'] == '13':
+        episode_id = i_episode['episode_id']
+
+
 my_req = requests.get('https://breakingbadapi.com/api/deaths')
 data = json.loads(my_req.text)
 max_death = 0
@@ -13,12 +21,11 @@ for j_elem in data:
             json.dump(j_elem, file, indent=4)
 with open('User_info.json', 'r') as file:
     data = json.load(file)
-print(data)
+
+print('ID episode:', episode_id)
 
 for i_info in data:
-    if i_info == 'death_id':
-        print('ID episode:', data[i_info])
-    elif i_info == 'season':
+    if i_info == 'season':
         print('Season number:', data[i_info])
     elif i_info == 'episode':
         print('Episode number:', data[i_info])
@@ -26,6 +33,3 @@ for i_info in data:
         print('Total deaths:', data[i_info])
     elif i_info == 'death':
         print('List of dead:', data[i_info])
-
-
-
